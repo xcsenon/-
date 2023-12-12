666677c6ec27c9b197930161160e0e31b6ce518d
vadim <vadim@XCSENON>
2023-12-12 15:37:57 +0200

    Добавьте файлы проекта.

---
+++ Фабричний метод/Фабричний метод.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+// Базовий клас продукту
+class Product {
+public:
+    virtual void display() = 0;
+};
+
+// Конкретний клас продукту
+class ConcreteProduct : public Product {
+public:
+    void display() override {
+        std::cout << "Concrete Product" << std::endl;
+    }
+};
+
+// Інтерфейс фабричного методу
+class Creator {
+public:
+    virtual Product* createProduct() = 0;
+};
+
+// Конкретний фабричний метод
+class ConcreteCreator : public Creator {
+public:
+    Product* createProduct() override {
+        return new ConcreteProduct();
+    }
+};
+
+int main() {
+    // Використання фабричного методу
+    Creator* creator = new ConcreteCreator();
+    Product* product = creator->createProduct();
+    product->display();
+
+    // Очищення пам'яті
+    delete product;
+    delete creator;
+
+    return 0;
+}
